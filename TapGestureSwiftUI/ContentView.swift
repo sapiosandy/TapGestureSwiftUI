fd3daae76b7eff9761fdfdb42c71ef73e2164d0a
Sandy Gomez <sgomez1029@gmail.com>
2024-03-04 19:21:13 -0800

    Initial Commit

---
+++ TapGestureSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapGestureSwiftUI
+//
+//  Created by Sandra Gomez on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapGestureSwiftUI/TapGestureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGestureSwiftUIApp.swift
+//  TapGestureSwiftUI
+//
+//  Created by Sandra Gomez on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TapGestureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
